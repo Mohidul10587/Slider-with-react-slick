79987c0ca60cc9313813eeed23e468d71fc07d9c
mohid <mohid10587@gmail.com>
2022-10-05 12:31:05 +0600

    Implement a slider with react-slick

---
+++ src/data.js
@@ -0,0 +1,100 @@
+export const dataDigitalBestSeller = [
+    {
+      id: 1,
+      title: ' 1 Mario Kart™ 8 Deluxe',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/m/mario-kart-8-deluxe-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 2,
+      title: '2 TRIANGLE STRATEGY™',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/t/triangle-strategy-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 3,
+      title: '3 Pokémon™ Legends: Arceus',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/p/pokemon-legends-arceus-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 4,
+      title: '4 Super Mario™ 3D World + Bowser’s Fury',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/s/super-mario-3d-world-plus-bowsers-fury-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 5,
+      title: '5 Cuphead',
+      price: '$19.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/c/cuphead-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 6,
+      title: '6 Minecraft',
+      price: '$29.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/m/minecraft-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 7,
+      title: '7 Mario + Rabbids® Kingdom Battle',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/u/unravel-two-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 8,
+      title: '8 Unravel Two',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      sale: 63, //percent
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/u/unravel-two-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 5,
+      title: '9 Cuphead',
+      price: '$19.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/c/cuphead-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 6,
+      title: '10 Minecraft',
+      price: '$29.99',
+      category: 'Nintendo Switch',
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/m/minecraft-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 7,
+      title: '11 Mario + Rabbids® Kingdom Battle',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      linkImg:
+      'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/u/unravel-two-switch/hero?_a=AJADJWI0',
+    },
+    {
+      id: 8,
+      title: '12 Unravel Two',
+      price: '$59.99',
+      category: 'Nintendo Switch',
+      sale: 63, //percent
+      linkImg:
+        'https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_auto/c_scale,w_300/v1/ncom/en_US/games/switch/u/unravel-two-switch/hero?_a=AJADJWI0',
+    },
+  ];
